1f4c3d0b39dd751a382f5e7b18ca588748fc0a8b
Sasha Yeltsov <sttorst78@gmail.com>
2020-12-07 12:11:33 +0200

    Changed Task3_1

--- Home_Work/Task3_1.py
+++ Home_Work/Task3_1.py
@@ -1,5 +1,25 @@
 #кількість входжень слів (better, never, is)
-str_python = "Now is better than never"
+str_python = """
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
 split_words = str_python.split()
 print ("split_words", split_words)
 
@@ -7,9 +27,7 @@ count1 = split_words.count('better')
 count2 = split_words.count('never')
 count3 = split_words.count('is')
 
-print ("The count better is", count1)
-print ("The count never is", count2)
-print ("The count is", count3)
+print (f"The count Better is {split_words.count('better')} the count Never is {split_words.count('never')} the count Is is {split_words.count('is')}")
 
 #Вивести весь текст у верхньому регістрі (всі великі літери)
 print (str_python.upper())
