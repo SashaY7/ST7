03cf1430802bb484ccfa82a8012cb2bb284056f1
Sasha Yeltsov <sttorst78@gmail.com>
2020-12-09 10:19:18 +0200

    Change Input

--- Home_Work/4_1.py
+++ Home_Work/4_1.py
@@ -1,5 +1,5 @@
-number1 = float(input("Введіть: number1 = " ))
-number2 = float(input("Введіть: number2 = " ))
+number1 = float(input("input: number1 = " ))
+number2 = float(input("input: number2 = " ))
 if number1 > number2:
     print(f"{number1} > {number2} ")
 elif number1 < number2:
