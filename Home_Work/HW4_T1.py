2c7971f9f47f812817704196d77f491ce634e47b
Sasha Yeltsov <sttorst78@gmail.com>
2020-12-10 20:05:08 +0200

    Aded HW4_t2.py

--- Home_Work/HW4_T1.py
+++ Home_Work/HW4_T1.py
@@ -1,3 +1,5 @@
+#Write a script that will calculate the factorial of the entered number  without using recursion.
+
 fact = int(input("Input number factorial: "))
 
 import math
---
+++ Home_Work/HW4_T2.py
@@ -0,0 +1,7 @@
+"[(print(float(i))) for i in range(1, 100)]"
+
+"""for number in range(100):
+    print(float(f"Out float number: {number}"))"""
+
+for i in range(1,100):
+    print(float(i))
