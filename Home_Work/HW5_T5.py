764fe8072b0cdbb2df32e0967049afa1f56fccee
Sasha Yeltsov <sttorst78@gmail.com>
2020-12-18 11:27:17 +0200

    HW5_T1

---
+++ Home_Work/HW5_T5.py
@@ -0,0 +1,7 @@
+pr = [number for number in range(1,10) if number % 2 == 0]
+npr = [number for number in range(1,10) if number % 3 == 0]
+other = [number for number in range(1,30) if not number %2 == 0 and not number %3 == 0]
+
+print("Parne: ", pr)
+print("Neparne", npr)
+print("Ne dil 2 and 3: ", other)
