15390a24f7a63b76a49e1f7495e1c05356ebaa86
Sasha Yeltsov <sttorst78@gmail.com>
2020-12-18 11:54:31 +0200

    HW5_T2

---
+++ Home_Work/HW5_T2.py
@@ -0,0 +1,10 @@
+login = ''
+while True:
+    print('What is you name?')
+    login = input()
+    if login == 'First':
+        print("Hello, First")
+        break
+    else:
+        print("It's mistake")
+        break
