df1718c5f9a59a6a5472b9085ec1c9917bcb6712
Sasha Yeltsov <sttorst78@gmail.com>
2020-12-18 10:28:01 +0200

    HW4_T3

--- Home_Work/HW4_T3.py
+++ Home_Work/HW4_T3.py
@@ -1,24 +1,13 @@
-"""def fibonacci(n):
-    a, b = 1, 1
-    for i in range(n):
-        yield a
-        a, b = b, a + b
+fib1 = 1
+fib2 = 1
+ 
+n = int(input("Input n: "))
 
-data = list(fibonacci(10))
-print(data)"""
-
-"""fib1 = fib2 = 1
-
-n = int(input("Input number Fibonacci: ")) - 2
-
-while n > 0:
-    fib1, fib2 = fib2, fib1 + fib2
-    n -= 1
-
-print(fib2) """
-########################################
-def fibonacci(n):
-    cur = 1
-    if n > 2:
-        cur = fibonacci(n-1) + fibonacci(n-2)
-    return cur
+i = 2 
+while i < n:
+    fib_sum = fib2 + fib1
+    fib1 = fib2
+    fib2 = fib_sum
+    i += 1
+ 
+print (fib_sum)
---
+++ Home_Work/HW4_T4.py
@@ -0,0 +1,6 @@
+my_list = ['p', 'r', 'o', 'b', 'l', 'e', 'm']
+my_list.remove('e')
+print(my_list)
+print(my_list.pop())
+del my_list[2]
+print(my_list)
